f6c298dacbcfe1e2cf2b4a7108a307f8e5fe0908
g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48 <g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48>
2010-10-24 10:40:40 +0000

    Updated script to do casual testing of 'CrossProduct' function after carrying several failed attempts at optimising it.
    --HG--
    extra : convert_revision : svn%3Ab091aa91-a314-92f1-5d41-19d272613d48/trunk%4055

--- development/solution/SageSerpent.Infrastructure/Script.fsx
+++ development/solution/SageSerpent.Infrastructure/Script.fsx
@@ -1,5 +1,9 @@
 #light
 
+#r "Z:/SageSerpent/workInProgress/development/solution/SageSerpent.Infrastructure/bin/Debug/SageSerpent.Infrastructure.dll"
+
+open SageSerpent.Infrastructure
+
 // This file is a script that can be executed with the F# Interactive.  
 // It can be used to explore and test the library project.
 // Note that script files will not be part of the project build.
@@ -10,3 +14,4 @@ let b = Map.of_list [(1, "Hi"); (3, "Rabbit!"); (2, "there")]
 
 let o = 2:>obj
 
+let p = BargainBasement.CrossProduct [[1;2;3];[67; -23];[0]]
