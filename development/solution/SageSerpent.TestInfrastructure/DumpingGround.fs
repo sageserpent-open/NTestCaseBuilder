d3389aab1cbfce22f7b794ffb1d3443a397be20a
g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48 <g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48>
2010-10-24 10:39:14 +0000

    Renamed the more confusing symbols in the 'Node' implementation.
    --HG--
    extra : convert_revision : svn%3Ab091aa91-a314-92f1-5d41-19d272613d48/trunk%4041

--- development/solution/SageSerpent.TestInfrastructure/DumpingGround.fs
+++ development/solution/SageSerpent.TestInfrastructure/DumpingGround.fs
@@ -11,7 +11,7 @@ namespace SageSerpent.TestInfrastructure
             TestVariableNode of seq<Object>
           | InterleavingNode of seq<Node>
           | SynthesizingNode of seq<Node>
-            member this.CombinationsOfTestLevelsOfStrengthUpToAndIncluding strength =
+            member this.CombinationsOfTestLevelsGroupedByStrengthUpToAndIncluding strength =
                 let rec walkTree node strength indexForLeftmostTestVariable =
                     if strength = 0
                     then raise (PreconditionViolationException "Strength must be non-zero.")
@@ -29,43 +29,47 @@ namespace SageSerpent.TestInfrastructure
                                       | headFromFirst::tailFromFirst, headFromSecond::tailFromSecond ->
                                             (Seq.append headFromFirst headFromSecond)::(joinPairsAtEachStrength tailFromFirst tailFromSecond)
                                 let mergeCombinationsFromSubtree (previouslyMergedCombinations, indexForLeftmostTestVariable) subtreeRootNode =
-                                    let combinationsFromSubtree, maximumTestVariableIndexFromSubtree =
+                                    let combinationsGroupedByStrengthFromSubtree, maximumTestVariableIndexFromSubtree =
                                         (walkTree subtreeRootNode strength indexForLeftmostTestVariable)
-                                    let mergedCombinations = joinPairsAtEachStrength previouslyMergedCombinations combinationsFromSubtree
-                                    mergedCombinations, maximumTestVariableIndexFromSubtree                                
+                                    let mergedCombinationsGroupedByStrength =
+                                        joinPairsAtEachStrength previouslyMergedCombinations combinationsGroupedByStrengthFromSubtree
+                                    mergedCombinationsGroupedByStrength, maximumTestVariableIndexFromSubtree                                
                                 subtreeRootNodes
                                 |> Seq.fold mergeCombinationsFromSubtree ([], indexForLeftmostTestVariable)
                             
                           | SynthesizingNode subtreeRootNodes ->
                                 let gatherCombinationsFromSubtree (prevouslyGatheredCombinations, indexForLeftmostTestVariable) subtreeRootNode =
-                                    let combinationsFromSubtree, maximumTestVariableIndexFromSubtree =
+                                    let combinationsGroupedByStrengthFromSubtree, maximumTestVariableIndexFromSubtree =
                                         (walkTree subtreeRootNode strength indexForLeftmostTestVariable)
-                                    let gatheredCombinations = combinationsFromSubtree::prevouslyGatheredCombinations
-                                    gatheredCombinations, maximumTestVariableIndexFromSubtree
+                                    let gatheredCombinationsGroupedBySubtreeAndThenByStrength =
+                                        combinationsGroupedByStrengthFromSubtree::prevouslyGatheredCombinations
+                                    gatheredCombinationsGroupedBySubtreeAndThenByStrength, maximumTestVariableIndexFromSubtree
                                 // Using 'fold' causes 'resultsFromSubtrees' to be built up in reverse to the subtree sequence,
                                 // and this reversal propagates consistently through the code below. The only place it could
                                 // cause a problem is where the maps are joined, but because the map joining is commutative *and*
                                 // because the test variable indices are already correctly calculated, it doesn't matter.
-                                let resultsFromSubtrees, maximumTestVariableIndex =
+                                let combinationsGroupedBySubtreeAndThenByStrength, maximumTestVariableIndex =
                                     subtreeRootNodes
                                     |> Seq.fold gatherCombinationsFromSubtree ([], indexForLeftmostTestVariable)
                                 let maximumStrengthsFromSubtrees =
-                                    resultsFromSubtrees
-                                    |> List.map (fun combinationsAtEachStrength -> uint32 (List.length combinationsAtEachStrength))
+                                    combinationsGroupedBySubtreeAndThenByStrength
+                                    |> List.map (fun combinationsForOneSubtreeGroupedByStrength
+                                                    -> uint32 (List.length combinationsForOneSubtreeGroupedByStrength))
                                 let overallStrength = List.max maximumStrengthsFromSubtrees
-                                let resultsFromSubtrees = List.map List.to_array resultsFromSubtrees
+                                let combinationsGroupedBySubtreeAndThenByStrength = List.map List.to_array combinationsGroupedBySubtreeAndThenByStrength
                                 let distributionsOfStrengthsOverSubtreesAtEachTotalStrength =
                                     CombinatoricUtilities.ChooseContributionsToMeetTotalsUpToLimit maximumStrengthsFromSubtrees overallStrength
                                 let addInCombinationsForGivenStrength partialResult strength distributions =
                                     let addInCombinationsForAGivenDistribution partialResult distribution =
                                         let combinationsBySubtree =
-                                            (List.zip distribution resultsFromSubtrees)
+                                            (List.zip distribution combinationsGroupedBySubtreeAndThenByStrength)
                                             |> List.map (function strength, resultFromSubtree -> resultFromSubtree.[int32 strength])
                                         let joinMaps first second =
                                             let keys map =
                                                 Set.of_seq (seq { for key in (map:> IDictionary<'Key, 'Value>).Keys do yield key })
                                             if not (Set.intersect (keys first) (keys second)).IsEmpty
-                                            then raise (InternalAssertionViolationException "Maps from test variable indices to levels contributed by separate subtrees should not share common keys.")
+                                            then raise (InternalAssertionViolationException
+                                                            "Maps from test variable indices to levels contributed by separate subtrees should not share common keys.")
                                             else Seq.append (Map.to_seq first) (Map.to_seq second)
                                                  |> Map.of_seq
                                         let combinationsBuiltFromCrossProduct =
