ef83e57f6b97190889803ec876f70a1bdf5b67ed
g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48 <g.j.murphy@sageserpent.com@b091aa91-a314-92f1-5d41-19d272613d48>
2012-02-13 22:38:20 +0000

    Added in unit tests for 'Unbounded<X>'.
    --HG--
    extra : convert_revision : svn%3Ab091aa91-a314-92f1-5d41-19d272613d48/trunk%40181

---
+++ development/solution/SageSerpent.Infrastructure.Tests/UnboundedTests.fs
@@ -0,0 +1,93 @@
+﻿namespace SageSerpent.Infrastructure.Tests
+
+    open NUnit.Framework
+
+    open SageSerpent.Infrastructure
+
+    [<TestFixture>]
+    type UnboundedTestFixture() =
+        let fortyFive =
+            Finite 45
+
+        let negativeInfinity =
+            NegativeInfinity
+
+        let positiveInfinity =
+            PositiveInfinity
+
+        let twentyThree =
+            Finite 23
+
+        let wrap x =
+            Finite x
+            
+
+        [<Test>]
+        member this.testFinitesAndInfinitesInCombination () =
+            Assert.IsTrue (negativeInfinity < fortyFive)
+
+            Assert.IsTrue (negativeInfinity < Finite 45)
+
+            Assert.IsTrue (NegativeInfinity < fortyFive)
+
+            Assert.IsTrue (NegativeInfinity < Finite 45)
+
+            Assert.IsTrue ((Finite 45) > negativeInfinity)
+
+            Assert.IsTrue ((Finite 45) > NegativeInfinity)
+
+            Assert.IsTrue (NegativeInfinity < wrap 45)
+
+            Assert.IsTrue((wrap 45) > NegativeInfinity)
+
+            Assert.IsTrue (NegativeInfinity < twentyThree)
+
+            let positiveInfinity = PositiveInfinity
+
+            Assert.IsTrue (positiveInfinity > fortyFive)
+
+            Assert.IsTrue (positiveInfinity > Finite 45)
+
+            Assert.IsTrue (PositiveInfinity > fortyFive)
+
+            Assert.IsTrue (PositiveInfinity > Finite 45)
+
+            Assert.IsTrue((Finite 45) < positiveInfinity)
+
+            Assert.IsTrue((Finite 45) < PositiveInfinity)
+
+            Assert.IsTrue (PositiveInfinity > wrap 45)
+
+            Assert.IsTrue((wrap 45) < PositiveInfinity)
+
+            Assert.IsTrue (PositiveInfinity > twentyThree)
+            
+        [<Test>]
+        member this.TestFinites() =
+            Assert.IsTrue (negativeInfinity < twentyThree)
+
+            Assert.IsTrue (twentyThree < fortyFive)
+
+            Assert.IsTrue (not (twentyThree > fortyFive))
+
+            Assert.IsTrue((Finite 23) = twentyThree)
+
+        [<Test>]
+        member this.TestInfinites() =
+            let shouldBeTrue =
+                NegativeInfinity = NegativeInfinity
+            Assert.IsTrue shouldBeTrue
+
+            Assert.IsTrue (NegativeInfinity <= NegativeInfinity && NegativeInfinity >= NegativeInfinity)
+            Assert.IsTrue (not (NegativeInfinity > NegativeInfinity || NegativeInfinity < NegativeInfinity))
+
+            let shouldBeTrue =
+                PositiveInfinity = PositiveInfinity
+            Assert.IsTrue shouldBeTrue
+
+            Assert.IsTrue (PositiveInfinity <= PositiveInfinity && PositiveInfinity >= PositiveInfinity)
+            Assert.IsTrue (not (PositiveInfinity > PositiveInfinity || PositiveInfinity < PositiveInfinity))
+
+            Assert.IsTrue (NegativeInfinity <> PositiveInfinity)
+            Assert.IsTrue (NegativeInfinity < PositiveInfinity)
+            Assert.IsTrue (PositiveInfinity > NegativeInfinity)
