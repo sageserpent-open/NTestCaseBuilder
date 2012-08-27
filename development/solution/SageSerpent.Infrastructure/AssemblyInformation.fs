702665e7fde11dce2f404ac3b8bc495a4a872b7c
Gerard Murphy <g.j.murphy@sageserpent.com>
2012-08-27 21:35:41 +0100

    Rationalise the assembly properties, which have previously been left as defaults. Starting at an official version of 1.2.0.0 for all assembly and file versions.

--- development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
+++ development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
@@ -1,7 +1,12 @@
 ﻿module SageSerpent.Infrastructure.DummyForAssemblyAttributes
     [<assembly: System.CLSCompliant(true);
       assembly: System.Runtime.InteropServices.ComVisible(false);
-      assembly: System.Reflection.AssemblyVersion("1.0.0.0")>]
+      assembly: System.Reflection.AssemblyVersion("1.2.0.0");
+      assembly: System.Reflection.AssemblyFileVersionAttribute("1.2.0.0");
+      assembly: System.Reflection.AssemblyDescriptionAttribute("Infrastructure library used and contributed by SageSerpent Ltd.");
+      assembly: System.Reflection.AssemblyProductAttribute("SageSerpent");
+      assembly: System.Reflection.AssemblyCompanyAttribute("SageSerpent Ltd");
+      assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright SageSerpent Ltd © 2012")>]
 
     do ()
 
