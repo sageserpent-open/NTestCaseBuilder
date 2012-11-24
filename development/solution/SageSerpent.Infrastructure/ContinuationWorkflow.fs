4e3cd84dd0d81be9f5d2a1de17c02d335a7229d6
Gerard Murphy <g.j.murphy@sageserpent.com>
2012-11-24 14:03:10 +0000

    Cutover 'ContinuationMonad' to using an object-oriented implementation rather rather than functions wrapped in a constructed type. This is horrible, but will hopefully gain some performance.

--- development/solution/SageSerpent.Infrastructure/ContinuationWorkflow.fs
+++ development/solution/SageSerpent.Infrastructure/ContinuationWorkflow.fs
@@ -1,57 +1,78 @@
 ﻿module SageSerpent.Infrastructure.ContinuationWorkflow
 
-    type ContinuationMonad<'Input, 'ExternalFinalResult> =
-        Step of (('Input -> 'ExternalFinalResult) * (unit -> 'ExternalFinalResult) -> 'ExternalFinalResult)
+    [<AbstractClass>]
+    type ContinuationMonad<'Input, 'ExternalFinalResult>() =
+        inherit Microsoft.FSharp.Core.FSharpFunc<('Input -> 'ExternalFinalResult) * (unit -> 'ExternalFinalResult), 'ExternalFinalResult>()
 
-            member inline this.Execute(successContinuation
-                                       , failureContinuation) =
-                match this with
-                    Step computation ->
-                        computation (successContinuation
-                                     , failureContinuation)
+        static member inline Execute(continuationMonad: ContinuationMonad<'FinalResult, 'FinalResult>) =
+            continuationMonad.Invoke (BargainBasement.Identity
+                                      , (fun () -> failwith "Unhandled failure: use the '+' operator to introduce an alternative computation for failure."))
 
-            static member inline Execute(continuationMonad: ContinuationMonad<'FinalResult, 'FinalResult>) =
-                continuationMonad.Execute(BargainBasement.Identity, (fun () -> failwith "Unhandled failure: use the '+' operator to introduce an alternative computation for failure."))
+        static member inline (+) (lhs: ContinuationMonad<'Input, 'ExternalFinalResult>,
+                                  rhs: ContinuationMonad<'Input, 'ExternalFinalResult>) =
+            {
+                new ContinuationMonad<'Input, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , failureContinuation)) =
+                        lhs.Invoke (successContinuation,
+                                    fun () ->
+                                        rhs.Invoke (successContinuation
+                                                    , failureContinuation))
+            }
 
-            static member inline (+) (lhs: ContinuationMonad<'Input, 'ExternalFinalResult>,
-                                      rhs: ContinuationMonad<'Input, 'ExternalFinalResult>) =
-                    Step (fun (successContinuation
-                               , failureContinuation) ->
-                            lhs.Execute(successContinuation,
-                                        fun () ->
-                                            rhs.Execute(successContinuation,
-                                                        failureContinuation)))
+        static member inline CallCC(body: ('Result -> ContinuationMonad<_, 'ExternalFinalResult>) -> ContinuationMonad<'Result, 'ExternalFinalResult>) =
+            {
+                new ContinuationMonad<'Result, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , failureContinuation)) =
+                        let ejectionSeat fastReturnResult =
+                            {
+                                new ContinuationMonad<_, 'ExternalFinalResult>() with
+                                    member this.Invoke ((_
+                                                         , _)) =
+                                        successContinuation fastReturnResult
+                            }
+                        (body ejectionSeat).Invoke (successContinuation
+                                                    , failureContinuation)
+            }
 
-            static member inline CallCC(body: ('Result -> ContinuationMonad<_, 'ExternalFinalResult>) -> ContinuationMonad<'Result, 'ExternalFinalResult>) =
-                Step (fun (successContinuation
-                           , failureContinuation) ->
-                    let ejectionSeat fastReturnResult =
-                        Step (fun (_, _) ->
-                                successContinuation fastReturnResult)
-                    (body ejectionSeat).Execute(successContinuation, failureContinuation))
-
-            static member inline CallCC ((exceptionHandler: 'Exception -> ContinuationMonad<'Result, 'ExternalFinalResult>),
-                                         (body: ('Exception -> ContinuationMonad<_, 'ExternalFinalResult>) -> ContinuationMonad<'Result, 'ExternalFinalResult>)) =
-                Step (fun (successContinuation
-                           , failureContinuation) ->
-                    let ejectionSeat exceptionReturn =
-                        Step (fun (_, _) ->
-                                (exceptionHandler exceptionReturn).Execute(successContinuation, failureContinuation))
-                    (body ejectionSeat).Execute(successContinuation, failureContinuation))
+        static member inline CallCC ((exceptionHandler: 'Exception -> ContinuationMonad<'Result, 'ExternalFinalResult>),
+                                     (body: ('Exception -> ContinuationMonad<_, 'ExternalFinalResult>) -> ContinuationMonad<'Result, 'ExternalFinalResult>)) =
+            {
+                new ContinuationMonad<'Result, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , failureContinuation)) =
+                        let ejectionSeat exceptionReturn =
+                            {
+                                new ContinuationMonad<_, 'ExternalFinalResult>() with
+                                    member this.Invoke ((_
+                                                         , _)) =
+                                        (exceptionHandler exceptionReturn).Invoke (successContinuation
+                                                                                   , failureContinuation)
+                            }
+                        (body ejectionSeat).Invoke (successContinuation
+                                                    , failureContinuation)
+            }
 
     type Builder () =
         member inline this.Bind (lhs: ContinuationMonad<'UnliftedLhsResult, 'ExternalFinalResult>,
                                  rhs: 'UnliftedLhsResult -> ContinuationMonad<'UnliftedRhsResult, 'ExternalFinalResult>): ContinuationMonad<'UnliftedRhsResult, 'ExternalFinalResult> =
-            Step (fun (successContinuation
-                       , failureContinuation) ->
-                    lhs.Execute((fun intermediateResult ->
-                                    (rhs intermediateResult).Execute(successContinuation, failureContinuation)),
-                                failureContinuation))
+            {
+                new ContinuationMonad<'UnliftedRhsResult, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , failureContinuation)) =
+                        lhs.Invoke ((fun intermediateResult ->
+                                        (rhs intermediateResult).Invoke (successContinuation, failureContinuation))
+                                    , failureContinuation)
+            }
 
         member inline this.Return (toBeLifted: 'Unlifted): ContinuationMonad<'Unlifted, 'ExternalFinalResult> =
-            Step (fun (successContinuation
-                       , _) ->
-                    successContinuation toBeLifted)
+            {
+                new ContinuationMonad<'Unlifted, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , _)) =
+                        successContinuation toBeLifted
+            }
 
         member inline this.ReturnFrom (alreadyLifted: ContinuationMonad<'Unlifted, 'ExternalFinalResult>): ContinuationMonad<'Unlifted, 'ExternalFinalResult> =
             alreadyLifted
@@ -61,18 +82,25 @@
             rhs lhs
 
         member inline this.Delay (delayedExpression: unit -> ContinuationMonad<'Input, 'ExternalFinalResult>) =
-            Step (fun (successContinuation
-                       , failureContinuation) ->
-                       (delayedExpression ()).Execute(successContinuation, failureContinuation))
+            {
+                new ContinuationMonad<'Input, 'ExternalFinalResult>() with
+                    member this.Invoke ((successContinuation
+                                         , failureContinuation)) =
+                        (delayedExpression ()).Invoke(successContinuation
+                                                      , failureContinuation)
+            }
 
         member inline this.Combine (lhs: ContinuationMonad<'Input, 'ExternalFinalResult>,
                                     rhs: ContinuationMonad<'Input, 'ExternalFinalResult>) =
             lhs + rhs
 
         member inline this.Zero (): ContinuationMonad<_, 'ExternalFinalResult> =
-            Step (fun (_
-                       , failureContinuation) ->
-                    failureContinuation ())
+            {
+                new ContinuationMonad<_, 'ExternalFinalResult>() with
+                    member this.Invoke ((_
+                                         , failureContinuation)) =
+                        failureContinuation ()
+            }
 
     let continuationWorkflow =
         Builder ()
