6fb4a62b650a81e48014dc7f5f02d617ba560e21
Gerard Murphy <g.j.murphy@sageserpent.com>
2013-02-03 20:20:07 +0000

    Push NuGet package 1.3.1.0 to NuGet.
    --HG--
    rename : development/solution/NTestCaseBuilder/NTestCaseBuilder.1.3.0.0.nupkg => development/solution/NTestCaseBuilder/NTestCaseBuilder.1.3.1.0.nupkg

--- development/solution/NTestCaseBuilder/AssemblyInformation.fs
+++ development/solution/NTestCaseBuilder/AssemblyInformation.fs
@@ -1,8 +1,8 @@
 ﻿module NTestCaseBuilder.DummyForAssemblyAttributes
     [<assembly: System.CLSCompliant(true);
       assembly: System.Runtime.InteropServices.ComVisible(false);
-      assembly: System.Reflection.AssemblyVersion("1.3.0.0");
-      assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0");
+      assembly: System.Reflection.AssemblyVersion("1.3.1.0");
+      assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.1.0");
       assembly: System.Reflection.AssemblyDescriptionAttribute("NTestCaseBuilder library.");
       assembly: System.Reflection.AssemblyProductAttribute("NTestCaseBuilder");
       assembly: System.Reflection.AssemblyCompanyAttribute("SageSerpent Ltd");
--- development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
+++ development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
@@ -1,8 +1,8 @@
 ﻿module SageSerpent.Infrastructure.DummyForAssemblyAttributes
     [<assembly: System.CLSCompliant(true);
       assembly: System.Runtime.InteropServices.ComVisible(false);
-      assembly: System.Reflection.AssemblyVersion("1.3.0.0");
-      assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0");
+      assembly: System.Reflection.AssemblyVersion("1.3.1.0");
+      assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.1.0");
       assembly: System.Reflection.AssemblyDescriptionAttribute("Infrastructure library used and contributed by SageSerpent Ltd.");
       assembly: System.Reflection.AssemblyProductAttribute("SageSerpent");
       assembly: System.Reflection.AssemblyCompanyAttribute("SageSerpent Ltd");
