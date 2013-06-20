2ba04ec3342ddd091002a97d90abb25cda079089
Gerard Murphy <g.j.murphy@sageserpent.com>
2013-06-20 18:31:45 +0100

    Document filtering permutations in API.
    Updated documentation after reviewing impact of filtering on existing
    unit tests.

--- development/solution/NTestCaseBuilder/TestCaseEnumerableFactory.fs
+++ development/solution/NTestCaseBuilder/TestCaseEnumerableFactory.fs
@@ -170,6 +170,8 @@
         /// to the position of the level in the sequence used to construct the test variable level factory for the level's test variable. This is useful, for example,
         /// for when two test variables share the same level and the filter is used to impose a constraint that the levels from both test variables can never be
         /// equal - comparing the indices allows a general-purpose filter to be written that avoids worrying about whether equality is defined for the level type.</remarks>
+        /// <remarks>If there is are any synthesizing nodes anywhere in the subtree covered by 'this' that perform permutations, the filter is taken to apply to
+        /// the test variable indices *before* the effect of the permutation - so filters do not have to be rewritten to take permutations into account.</remarks>
         /// <param name="filter">Delegate accepting a dictionary that describes the combination of levels being considered - each key in the dictionary is a test
         /// variable index denoting one of the test variables involved; the associated value is a pair of an index denoting the actual test level for that test
         /// variable in the combination, together with the value of the test level itself. Returns true if the combination of levels is permitted, false if the combination
