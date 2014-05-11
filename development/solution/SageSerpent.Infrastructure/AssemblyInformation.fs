970439cd6679d06adaf127efdbec27e1f33ca4d4
gerardMurphy <gerardMurphy@.(none)>
2014-05-11 20:56:52 +0100

    Produce NTestCaseBuilder 2.5.0.0

--- development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
+++ development/solution/SageSerpent.Infrastructure/AssemblyInformation.fs
@@ -1,12 +1,12 @@
 ﻿module SageSerpent.Infrastructure.DummyForAssemblyAttributes
     [<assembly: System.CLSCompliant(true);
       assembly: System.Runtime.InteropServices.ComVisible(false);
-      assembly: System.Reflection.AssemblyVersion("2.4.0.0");
-      assembly: System.Reflection.AssemblyFileVersionAttribute("2.4.0.0");
+      assembly: System.Reflection.AssemblyVersion("2.5.0.0");
+      assembly: System.Reflection.AssemblyFileVersionAttribute("2.5.0.0");
       assembly: System.Reflection.AssemblyDescriptionAttribute("Infrastructure library used and contributed by SageSerpent Ltd.");
       assembly: System.Reflection.AssemblyProductAttribute("SageSerpent");
       assembly: System.Reflection.AssemblyCompanyAttribute("SageSerpent Ltd");
-      assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright SageSerpent Ltd © 2013")>]
+      assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright SageSerpent Ltd © 2014")>]
 
     do ()
 
