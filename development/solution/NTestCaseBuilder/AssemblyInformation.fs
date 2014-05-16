f3e8cfca2c7bca05391f5992549d1945846b54aa
gerardMurphy <gerardMurphy@.(none)>
2014-05-16 05:56:17 +0100

    Produce NTestCaseBuilder 2.5.1.0

--- development/solution/NTestCaseBuilder/AssemblyInformation.fs
+++ development/solution/NTestCaseBuilder/AssemblyInformation.fs
@@ -1,8 +1,8 @@
 ﻿module NTestCaseBuilder.DummyForAssemblyAttributes
     [<assembly: System.CLSCompliant(true);
       assembly: System.Runtime.InteropServices.ComVisible(false);
-      assembly: System.Reflection.AssemblyVersion("2.5.0.0");
-      assembly: System.Reflection.AssemblyFileVersionAttribute("2.5.0.0");
+      assembly: System.Reflection.AssemblyVersion("2.5.1.0");
+      assembly: System.Reflection.AssemblyFileVersionAttribute("2.5.1.0");
       assembly: System.Reflection.AssemblyDescriptionAttribute("NTestCaseBuilder library.");
       assembly: System.Reflection.AssemblyProductAttribute("NTestCaseBuilder");
       assembly: System.Reflection.AssemblyCompanyAttribute("SageSerpent Ltd");
